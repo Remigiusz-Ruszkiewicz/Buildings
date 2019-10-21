586bfebec58100c2a472217756f414f5a0cac181
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-10-21 18:55:44 +0200

    Add project files.

---
+++ Buildings/Building.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buildings
+{
+    class Building
+    {
+        public string Name { get; set; }
+        private List<Floar>;
+        private string Destiny;
+        private DateTime Year;
+        public Floor floor;
+        public class Floar
+        {
+            public string Name { get; set; }
+            private int FloorNumber;
+        }
+    }
+}
